5320375ff146a655fa9edc55e74e2b2e0dd3df72
Ivan Saldano <ivanms1@gmail.com>
2020-04-08 09:08:35 -0400

    remove tv from navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -5,14 +5,14 @@ import styles from './Navbar.module.css';
 const Navbar = () => {
   return (
     <div className={styles.Navbar}>
+      <Link href='/'>
+        <a>Home</a>
+      </Link>
       <Link href='/popular'>
         <a>Most popular</a>
       </Link>
       <Link href='/search'>
         <a>Search</a>
-      </Link>
-      <Link href='/tv'>
-        <a>TV</a>
       </Link>
     </div>
   );
